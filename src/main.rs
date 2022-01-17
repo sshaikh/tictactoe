436928e49f0fe138600adc0876dc1d8e33b6d3a8
Shak <shak@atal.cc>
2022-01-17 17:16:39 +0000

    first try

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+#[derive(Clone, Copy)]
+enum Counter {
+    X,
+    O,
+}
+
+struct Board {
+    width: usize,
+    height: usize,
+    state: [[Counter; 3]; 3]
+}
+
+impl Board {
+    pub fn get(&self, row: usize, col: usize) -> Option<Counter> {
+        Some(*self.state.get(row)?.get(col)?)
+    }
+}
+
