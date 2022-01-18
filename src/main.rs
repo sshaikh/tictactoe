f124660886ede021e41f99c425d4085274c855d6
Shak <shak@atal.cc>
2022-01-18 17:58:08 +0000

    started again without structs (doesn't compile!)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,94 @@
+use std::cmp;
+
+type State = Vec<Vec<char>>;
+
+const _WIDTH: usize = 3;
+const _HEIGHT: usize = 3;
+
 fn main() {
-    println!("Hello, world!");
+
+
+    let _initial_board = vec![vec![' '; _WIDTH]; _HEIGHT];
+    
+    let first_turn = generate_next_turns(_initial_board, 'X');
+
+    println!("{:?}", first_turn);
+
+    
+
 }
 
+fn generate_next_turns(state: State, player: char) -> Vec<State> {
 
-#[derive(Clone, Copy)]
-enum Counter {
-    X,
-    O,
-}
-
-struct Board {
-    width: usize,
-    height: usize,
-    state: [[Counter; 3]; 3]
-}
-
-impl Board {
-    pub fn get(&self, row: usize, col: usize) -> Option<Counter> {
-        Some(*self.state.get(row)?.get(col)?)
+    let mut res = Vec::new();
+    for (rownum, row) in state.iter().enumerate() {
+    for (colnum, &c) in row.iter().enumerate() {
+        if c == ' ' {
+            let mut next = state.clone();
+            next[rownum][colnum] = player;
+            res.push(next);
+        }
     }
+    }
+
+    res
+
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum TicTacToeState{
+    Intermediate,
+    Won(char),
+    Draw
+}
+
+fn check_win_for(state: State, player: char) -> bool {
+    // check rows
+    for row in &state {
+        if row.iter().all(|&c| c == player) {
+            return true;
+        }
+    }
+    //check columns
+    for colnum in 0.._WIDTH {
+        if state.iter().all(|row| row[colnum] == player) {
+            return true;
+        }
+    }
+    //check forward diagonals
+    for colnum in 0..(_WIDTH - _HEIGHT) {
+        for rownum in 0..(_HEIGHT - _WIDTH) {
+            let mut result = true;
+            for offset in 0..cmp::min(_WIDTH, _HEIGHT) {
+                if state[rownum+offset][colnum+offset] != player {
+                    result = false;
+                    break;
+                }
+            }
+            if result {return true}
+        }
+    }
+    //check backward diagonals
+    for colnum in _WIDTH-1.._HEIGHT {
+        for rownum in 0..(_HEIGHT - _WIDTH) {
+            let mut result = true;
+            for offset in 0..cmp::min(_WIDTH, _HEIGHT) {
+                if state[rownum + offset][colnum - offset] != player {
+                    result = false;
+                    break;
+                }
+            }
+            if result {return true}
+        }
+    }
+    false
+}
+
+fn get_end_state(state: State) -> TicTacToeState {
+    check_win_for(state, 'X');
+    TicTacToeState::Draw
+}
+
+fn is_end_state(state: State) -> bool{
+    get_end_state(state) != TicTacToeState::Intermediate
+}
