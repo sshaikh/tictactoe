fb62e3a05b2c0928a00f51ee9ca857d9184993f3
Shak <shak@atal.cc>
2022-01-20 18:30:45 +0000

    reordered functions

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ struct State {
     path: Vec<u8>,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum TicTacToeState {
+    Intermediate,
+    Draw,
+    XWin,
+    OWin,
+}
+
 fn main() {
     let initial_board = State {
         board: vec![vec![' '; 3]; 3],
@@ -21,6 +29,75 @@ fn main() {
         states.get(&TicTacToeState::XWin).unwrap().len(),
         states.get(&TicTacToeState::OWin).unwrap().len()
     );
+}
+
+fn get_ord_from_coords(row: usize, col: usize) -> u8 {
+    u8::try_from(col + (3 * row) + 1).unwrap()
+}
+
+fn generate_next_turns(state: State, player: char) -> Vec<State> {
+    let mut res = Vec::new();
+    for (rownum, row) in state.board.iter().enumerate() {
+        for (colnum, &c) in row.iter().enumerate() {
+            if c == ' ' {
+                let mut next = state.clone();
+                next.board[rownum][colnum] = player;
+                next.path.push(get_ord_from_coords(rownum, colnum));
+                res.push(next);
+            }
+        }
+    }
+    res
+}
+
+fn check_win_for(state: &Vec<Vec<char>>, player: char) -> bool {
+    // check rows
+    for row in state {
+        if row.iter().all(|&c| c == player) {
+            return true;
+        }
+    }
+    //check columns
+    for colnum in 0..3 {
+        if state.iter().all(|row| row[colnum] == player) {
+            return true;
+        }
+    }
+    //check diagonals
+    (state[0][0] == player && state[1][1] == player && state[2][2] == player)
+        || (state[2][0] == player && state[1][1] == player && state[0][2] == player)
+}
+
+fn get_end_state(state: &State) -> TicTacToeState {
+    if check_win_for(&state.board, 'X') {
+        return TicTacToeState::XWin;
+    }
+    if check_win_for(&state.board, 'O') {
+        return TicTacToeState::OWin;
+    }
+    if state.path.len() == 9 {
+        return TicTacToeState::Draw;
+    }
+    TicTacToeState::Intermediate
+}
+
+fn classify(states: Vec<State>) -> HashMap<TicTacToeState, Vec<State>> {
+    let mut result = HashMap::new();
+    result.insert(TicTacToeState::Intermediate, Vec::new());
+    result.insert(TicTacToeState::Draw, Vec::new());
+    result.insert(TicTacToeState::XWin, Vec::new());
+    result.insert(TicTacToeState::OWin, Vec::new());
+    for state in states {
+        result.get_mut(&get_end_state(&state)).unwrap().push(state);
+    }
+    result
+}
+
+fn get_next_player(player: char) -> char {
+    if player == 'X' {
+        return 'O';
+    }
+    'X'
 }
 
 fn print_states(turn: u8, states: &HashMap<TicTacToeState, Vec<State>>) {
@@ -61,81 +138,4 @@ fn play_games(states: Vec<State>, player: char, turn: u8) -> HashMap<TicTacToeSt
             .append(result.get_mut(&TicTacToeState::OWin).unwrap());
     }
     classified
-}
-
-fn get_next_player(player: char) -> char {
-    if player == 'X' {
-        return 'O';
-    }
-    'X'
-}
-
-fn classify(states: Vec<State>) -> HashMap<TicTacToeState, Vec<State>> {
-    let mut result = HashMap::new();
-    result.insert(TicTacToeState::Intermediate, Vec::new());
-    result.insert(TicTacToeState::Draw, Vec::new());
-    result.insert(TicTacToeState::XWin, Vec::new());
-    result.insert(TicTacToeState::OWin, Vec::new());
-    for state in states {
-        result.get_mut(&get_end_state(&state)).unwrap().push(state);
-    }
-    result
-}
-
-fn get_ord_from_coords(row: usize, col: usize) -> u8 {
-    u8::try_from(col + (3 * row) + 1).unwrap()
-}
-
-fn generate_next_turns(state: State, player: char) -> Vec<State> {
-    let mut res = Vec::new();
-    for (rownum, row) in state.board.iter().enumerate() {
-        for (colnum, &c) in row.iter().enumerate() {
-            if c == ' ' {
-                let mut next = state.clone();
-                next.board[rownum][colnum] = player;
-                next.path.push(get_ord_from_coords(rownum, colnum));
-                res.push(next);
-            }
-        }
-    }
-    res
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-enum TicTacToeState {
-    Intermediate,
-    Draw,
-    XWin,
-    OWin,
-}
-
-fn check_win_for(state: &Vec<Vec<char>>, player: char) -> bool {
-    // check rows
-    for row in state {
-        if row.iter().all(|&c| c == player) {
-            return true;
-        }
-    }
-    //check columns
-    for colnum in 0..3 {
-        if state.iter().all(|row| row[colnum] == player) {
-            return true;
-        }
-    }
-    //check forward diagonals
-    (state[0][0] == player && state[1][1] == player && state[2][2] == player)
-        || (state[2][0] == player && state[1][1] == player && state[0][2] == player)
-}
-
-fn get_end_state(state: &State) -> TicTacToeState {
-    if check_win_for(&state.board, 'X') {
-        return TicTacToeState::XWin;
-    }
-    if check_win_for(&state.board, 'O') {
-        return TicTacToeState::OWin;
-    }
-    if state.path.len() == 9 {
-        return TicTacToeState::Draw;
-    }
-    TicTacToeState::Intermediate
 }
